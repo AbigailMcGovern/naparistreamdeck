307b954c74d875ce0d1b479098cf6b1cd6be6586
AbigailMcGovern <abigail_mcgovern@hotmail.com>
2024-01-11 11:03:08 +1100

    draft code for touchscreen short and long - layerview 1

--- src/naparistreamdeck/touchscreen_callbacks.py
+++ src/naparistreamdeck/touchscreen_callbacks.py
@@ -23,14 +23,68 @@ class TouchScreen:
         self.viewer = viewer
         self.screendims = (100, 800)
         self.screen_page = 0
+        self.build_and_set_screens() # sets layerlist_screen, axis_screen, and dial_screen attributes
+        self.build_touchscreen_image() # sets touchscreen_image attribute
+        self.screen_lut = self.get_shorttouch_screen_LUT() # list of array
+        self.dialview_lut = self.get_dialview_LUT()
+        
+
+    def build_and_set_screens(self):
+        """
+        Builds and sets the screens for the current layer type and layerout
+        """
         self.layerlist_screen = self.build_layerlist_screen() # list of array
         self.axis_screen = self.build_axis_screen() # array
         self.dial_screen = self.build_dial_screen() # list of array
-        self.screen_lut = self.get_shorttouch_screen_LUT() # list of array
-        self.dialview_lut = self.get_dialview_LUT()
+
+    
+    def update_axis_screen(self):
         pass
 
 
+    def build_touchscreen_image(self):
+        """
+        Build and set the touchscreen image for the current page displayed. 
+        
+        TODO: finish this
+        """
+        self.update_axis_screen()
+        self.touchscreen_image = ...
+        
+
+    def build_layerlist_screen(self):
+        """
+        Generate the RGB array representing the image on the top part of the touch screen
+        (i.e., a ... x ... pixel area). The image comprises the layer thumbnails on 
+        a grey background (the colour of the napari window with the default theme). 
+        If there are too many thumbnails, several screens will be built and stored. 
+
+        TODO: connect this to callbacks indicating that the layer list has changed or
+        the current selection has changed. 
+        """
+
+        # we will have four 90 x 200 pixel regions for up to four layers on the screen at
+        # any one time. There will be a 2 pixel boarder around each one. The selected layer
+        # will have a white boarder and the others will have grey. For each layer, there 
+        # will be a 86 x 86 thumbnail on the left hand side. On the other side there will
+        # be a slightly lighter gray region (matching they layerlist icons) with the index
+        # of the layer displayed. 
+        pass
+
+
+    def build_axis_screen(self):
+        """
+        Generate the RGB array representing the bottom part of the touch screen. 
+        On this part of the screen, the user can see the location of the sliders on any
+        axes currently opperated by sliders. 
+        """
+        pass
+
+
+    def build_dial_screen(self):
+        pass
+
+    
     def get_shorttouch_screen_LUT(self):
         """
         Using the layerlist, information about axis display, and the current page
@@ -75,40 +129,6 @@ class TouchScreen:
         # if the current layer is a labels layer
         if isinstance(self.viewer.layers.selection.active, nlabs.Labels):
             pass
-        
-
-
-    def build_layerlist_screen(self):
-        """
-        Generate the RGB array representing the image on the top part of the touch screen
-        (i.e., a ... x ... pixel area). The image comprises the layer thumbnails on 
-        a grey background (the colour of the napari window with the default theme). 
-        If there are too many thumbnails, several screens will be built and stored. 
-
-        TODO: connect this to callbacks indicating that the layer list has changed or
-        the current selection has changed. 
-        """
-
-        # we will have four 90 x 200 pixel regions for up to four layers on the screen at
-        # any one time. There will be a 2 pixel boarder around each one. The selected layer
-        # will have a white boarder and the others will have grey. For each layer, there 
-        # will be a 86 x 86 thumbnail on the left hand side. On the other side there will
-        # be a slightly lighter gray region (matching they layerlist icons) with the index
-        # of the layer displayed. 
-        pass
-
-
-    def build_axis_screen(self):
-        """
-        Generate the RGB array representing the bottom part of the touch screen. 
-        On this part of the screen, the user can see the location of the sliders on any
-        axes currently opperated by sliders. 
-        """
-        pass
-
-
-    def build_dial_screen(self):
-        pass
 
 
     def change_active_layer(self, value):
@@ -127,5 +147,12 @@ class TouchScreen:
         elif self.view == 'dialview':
             pass
 
+
+    def drag_event(self, value):
+        if self.view == 'layerview':
+            pass
+
+        elif self.view == 'dialview':
+            pass
 
 
